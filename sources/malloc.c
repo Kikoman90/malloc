305fc92c5c2bf6caf29410ef53e5b3673457202e
Francois SIDLER <fsidler@e1r12p15.42.fr>
2019-01-04 18:17:29 +0100

    koala

--- sources/malloc.c
+++ sources/malloc.c
@@ -1,11 +1,13 @@
 #include "malloc.h"
 
-void		*mymalloc(size_t size) {
-	void    *mem;
+void		*mymalloc(size_t size)
+{
+	void	*ptr;
 
-    if (!size && size > LARGE_SIZE)
-        return NULL;
-    if ((mem = mmap(0, size + 1, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0) == MAP_FAILED)
-        return NULL;
-    return (mem);
+	if (!size || size > LARGE_SIZE)
+		return (NULL);
+	if ((mem = mmap(0, size + 1, PROT_READ | PROT_WRITE, \
+		MAP_ANON | MAP_PRIVATE, -1, 0)) == MAP_FAILED)
+		return (NULL);
+	return (ptr);
 }
