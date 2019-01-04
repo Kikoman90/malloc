ae2a8fbe78da6732b5d9a3e6e9ade105f7ca97b3
Jayson MERLINI <jamerlin@e1r12p16.42.fr>
2019-01-04 18:02:58 +0100

    malloc start

--- sources/malloc.c
+++ sources/malloc.c
@@ -1,8 +1,9 @@
 #include "malloc.h"
 
 void		*malloc(size_t size) {
-	void	*ptr;
-
-	ptr = NULL;
-	return (ptr);
+	void *mem;
+    if (size < 1)
+        return NULL;
+    mem = mmap(0, size + 1, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+    return (mem);
 }
