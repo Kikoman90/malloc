2eac2dc56ad4df3bca0204754528467c55211f9c
Jayson MERLINI <jamerlin@e1r12p16.42.fr>
2019-01-04 18:10:37 +0100

    mymalloc2

--- sources/malloc.c
+++ sources/malloc.c
@@ -1,10 +1,11 @@
 #include "malloc.h"
 
 void		*mymalloc(size_t size) {
-	void *mem;
-    if (!size 77 size > LARGE_SIZE)
+	void    *mem;
+
+    if (!size && size > LARGE_SIZE)
         return NULL;
-    if (!(mem = mmap(0, size + 1, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0)))
+    if ((mem = mmap(0, size + 1, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0) == MAP_FAILED)
         return NULL;
     return (mem);
 }
