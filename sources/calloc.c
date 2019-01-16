e33a5134c7dbdef488783cbef093df100c43bac8
Jayson MERLINI <jamerlin@e1r5p4.42.fr>
2019-01-16 17:29:37 +0100

    calloc for test

---
+++ sources/calloc.c
@@ -0,0 +1,27 @@
+#include "malloc.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t	i;
+	char	*buff;
+
+	buff = (char *)b;
+	i = 0;
+	while (i < len)
+	{
+		buff[i] = c;
+		i++;
+	}
+	return (buff);
+}
+
+void    *calloc(size_t nmemb, size_t size)
+{
+    void *ptr;
+
+    if (nmemb == 0)
+        return (NULL);
+    if (ptr = mymalloc(nmemb * size) != NULL)
+        ft_memset(ptr, 0, (nmemb * size));
+    return (ptr);
+}
